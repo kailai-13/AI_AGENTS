f6cc288e723522aa1222a820a290c2226bf71111
kailai-13 <kailainathan2006@gmail.com>
2025-08-17 12:10:22 +0530

    initials

--- te.py
+++ te.py
@@ -1,7 +1,5 @@
 try:
-    from concordia.components import agent as agent_components
-    from concordia.associative_memory import associative_memory
-    from concordia.language_model import language_model
+    import concordia
     HAVE_CONCORDIA = True
 except Exception:  # Concordia not installed
     HAVE_CONCORDIA = False
